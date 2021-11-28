fa69ebf71d130b157ca4eeac8862b788fcd6c13a
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-11-27 20:17:39 -0700

    added index.js

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -2,7 +2,7 @@ const { User } = require('../models');
 
 const userController = {
 
-    createUser: ({ body }, res) {
+    createUser ({ body }, res) {
         User.create(body)
         .then(dbUserData => res.json(dbUserData))
         .catch(err => res.status(400).json(err));
