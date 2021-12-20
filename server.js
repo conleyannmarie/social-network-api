7b3bb15198be212ef86215b65213075e43ad6a2f
Conley Marie <81729253+conleyannmarie@users.noreply.github.com>
2021-12-19 19:01:02 -0700

    added fixes

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const mongoose = require('mongoose');
-
-const routes = require('./routes');
+const express = require('express');
+const mongoose = require('mongoose')
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -12,9 +12,15 @@ app.use(require('./routes'))
 
 const { User } = require('./models');
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mongoose', {
+
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 // Use this to log mongo queries being executed!
 mongoose.set('debug', true)
 
-app.listen(PORT, () => console.log(`Connected on localhost:${PORT}`));
+
+app.listen(PORT, () => console.log(`🌍 Connected on localhost:${PORT}`));
